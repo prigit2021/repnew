0c150d003ede54165fe6cb96a11f6b2990a5c91d
prigit2021 <priya21.boopathy@gmail.com>
2021-11-09 15:19:21 +0530

    Folder structure

---
+++ src/org/fin/Username.java
@@ -0,0 +1,13 @@
+package org.fin;
+
+public class Username {
+public static void login()
+{
+System.out.println("my name is priya");
+}
+	public static void main(String[] args) {
+		login();
+
+	}
+
+}
